7ecad7a719ec41c0a34dd36e5d58deec70de986f
Ginofvs <gvillavicenciosifuentes@gmail.com>
2024-05-01 21:50:15 -0500

    primer commit

---
+++ flutter_application_241124/lib/servicios/database.dart
@@ -0,0 +1,8 @@
+//
+// Ver este tema d  firebase
+
+// class DatabaseMethods{
+//   Future addTodayWork(Map<String,dynamic> userTodayMap, String id)async{
+//     return await Firebase
+//   }
+// }
